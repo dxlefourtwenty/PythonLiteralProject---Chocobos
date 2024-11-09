0cace93335b05a2122b6b736c70f89e8e5a1c785
Dale Peligro <daledoesminecraft2013@gmail.com>
2024-11-09 02:43:10 -0800

    Fully implemented working NFA (second version)

--- NFA.java
+++ NFA.java
@@ -10,29 +10,46 @@ public class NFA {
     // Constructor
     public NFA(Set<String> states, Set<Character> alphabet, Map<String, Map<Character, Set<String>>> transitions,
                String startState, Set<String> acceptStates) {
-        this.states = states;
-        this.alphabet = alphabet;
-        this.transitions = transitions;
+        this.states = new LinkedHashSet<>(states);
+        this.alphabet = new LinkedHashSet<>(alphabet);
+        this.transitions = new LinkedHashMap<>();
+
+        for (String state : transitions.keySet()) {
+            Map<Character, Set<String>> stateTransitions = new LinkedHashMap<>();
+            for (Character symbol : transitions.get(state).keySet()) {
+                stateTransitions.put(symbol, new LinkedHashSet<>(transitions.get(state).get(symbol)));
+            }
+            this.transitions.put(state, stateTransitions);
+        }
+
         this.startState = startState;
-        this.acceptStates = acceptStates;
+        this.acceptStates = new LinkedHashSet<>(acceptStates);
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
 
-        sb.append("States: ").append(states).append("\n");
+        List<String> sortedStates = new ArrayList<>(states);
+        Collections.sort(sortedStates);
+        sb.append("States: ").append(sortedStates).append("\n");
         sb.append("Alphabet: ").append(alphabet).append("\n");
         sb.append("Start State: ").append(startState).append("\n");
         sb.append("Accept States: ").append(acceptStates).append("\n");
         sb.append("Transitions:\n");
 
-        for (String state : transitions.keySet()) {
-            for (Character symbol : transitions.get(state).keySet()) {
-                Set<String> nextStates = transitions.get(state).get(symbol);
-                String symbolStr = (symbol == null) ? "ε" : symbol.toString();  // ε represents epsilon
-                sb.append("  ").append(state).append(" --").append(symbolStr)
-                  .append("--> ").append(nextStates).append("\n");
+        List<String> sortedTransitions = new ArrayList<>(transitions.keySet());
+        Collections.sort(sortedTransitions);
+
+        for (String state : sortedTransitions) {
+            List<Character> sortedSymbols = new ArrayList<>(transitions.get(state).keySet());
+            Collections.sort(sortedSymbols, Comparator.nullsFirst(Comparator.naturalOrder()));
+
+            for (Character symbol : sortedSymbols) {
+            Set<String> nextStates = transitions.get(state).get(symbol);
+            String symbolStr = (symbol == null) ? "ε" : symbol.toString();  // ε represents epsilon
+            sb.append("  ").append(state).append(" --").append(symbolStr)
+              .append("--> ").append(nextStates).append("\n");
             }
         }
     
--- PythonLiteral.java
+++ PythonLiteral.java
@@ -1,5 +1,3 @@
-import java.util.Set;
-import java.util.HashSet;
 import java.util.*;
 
 public class PythonLiteral {
@@ -13,28 +11,31 @@ public class PythonLiteral {
         Map<String, Map<Character, Set<String>>> transitions = new HashMap<>();
 
         // q0 --a--> {q0, q1}
-        transitions.put("q0", new HashMap<>());
-        transitions.get("q0").put('a', new HashSet<>(Arrays.asList("q0", "q1")));
+        Map<Character, Set<String>> q0Transitions = new HashMap<>();
+        q0Transitions.put('a', new LinkedHashSet<>(Arrays.asList("q0", "q1")));
+        transitions.put("q0", q0Transitions);
 
         // q1 --b--> {q2}
-        transitions.put("q1", new HashMap<>());
-        transitions.get("q1").put('b', new HashSet<>(Collections.singletonList("q2")));
+        Map<Character, Set<String>> q1Transitions = new HashMap<>();
+        q1Transitions.put('b', new LinkedHashSet<>(Collections.singletonList("q2")));
+        transitions.put("q1", q1Transitions);
 
         // q2 --epsilon--> {q0}
-        transitions.put("q2", new HashMap<>());
-        transitions.get("q2").put('b', new HashSet<>(Collections.singletonList("q0")));
+        Map<Character, Set<String>> q2Transitions = new HashMap<>();
+        q2Transitions.put(null, new LinkedHashSet<>(Collections.singletonList("q0")));
+        transitions.put("q2", q2Transitions);
 
         String startState = "q0";
-        Set<String> acceptStates = new HashSet<>(Collections.singletonList("q2"));
+        Set<String> acceptStates = new LinkedHashSet<>(Collections.singletonList("q2"));
 
         // Instantiate the NFA
         NFA nfa = new NFA(states, alphabet, transitions, startState, acceptStates);
 
         
         System.out.println(nfa);
-        System.out.println(nfa.accept("ab"));
-        System.out.println(nfa.accept("aaab"));
-        System.out.println(nfa.accept("ba"));
+        System.out.println("Accepts 'ab' >> " + nfa.accept("ab"));
+        System.out.println("Accepts 'aaab' >> " + nfa.accept("aaab"));
+        System.out.println("Accepts 'ba' >> " + nfa.accept("ba"));
     }
 
 }
